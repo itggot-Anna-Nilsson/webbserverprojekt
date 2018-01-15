4e54c7e98ea8e955305e77710dcf00cabb013987
Anna <anna.nilsson@itggot.se>
2018-01-15 09:46:20 +0100

    Uppdaterade get.rb

--- get.rb
+++ get.rb
@@ -3,10 +3,10 @@ class Get < Sinatra::Base
     enable :sessions
 
     get '/'
-    if session[:admin]
-        @admin = true
-    end
-    slim :'login'
+    # if session[:admin]
+    #     @admin = true
+    # end
+    # slim :'login'
     end
 
 end    
