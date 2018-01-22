d269d76bd11b6bb69c53cab61e51b0329682fa62
Anna <anna.nilsson@itggot.se>
2018-01-22 15:13:21 +0100

    Updaterade get

--- get.rb
+++ get.rb
@@ -9,26 +9,18 @@ class Get < Sinatra::Base
         slim :'login'
     end
     
-    get '/home' do
+    get '/start' do
         if session[:admin]
-            slim :'home'
+            slim :'start'
         else
             halt 401, slim(:forbidden, layout: false)
         end
     end
 
     post '/login' do #test - 123
-        db = SQLite3::Database.open('db/db.sqlite')        
         username = params['username']
         password = params['password']
-        hash = db.execute('SELECT password FROM Users WHERE username IS ?', username)[0][0]
-        stored_password = BCrypt::Password.new(hash)
-        if stored_password == username + password
-            session[:admin] = true
-            session[:username] = username
-            redirect '/start'
-        end
-        redirect '/'
+        User.login(username, password, self)
     end
 
     post '/new_user' do
@@ -37,12 +29,15 @@ class Get < Sinatra::Base
         password = params['password']
         key = params['key']
 
-        username_list = db.execute('SELECT username FROM Users')[0]
+        username_list = db.execute('SELECT name FROM Users')[0]
         unused_username = true
 
-        for i in username_list
-            if username == i
-                unused_username = false
+        p username_list
+        if username_list != nil
+            for i in username_list
+                if username == i
+                    unused_username = false
+                end
             end
         end
 
@@ -50,10 +45,10 @@ class Get < Sinatra::Base
             cleartext = username + password
             hash = BCrypt::Password.create(cleartext)
             salt = hash.salt
-            db.execute('INSERT INTO users (password, salt, username) VALUES(?,?,?)', [hash, salt, username])
+            db.execute('INSERT INTO users (name, hash) VALUES(?,?)', [username, hash])
             session[:admin] = true
             session[:username] = username
-            redirect '/home'
+            redirect '/start'
         else 
             redirect '/wrongkey'
         end
